e45fdecfe799fc685860af6fec7e24b38fb0dd40
Darren_Ter <tzyong990808@gmail.com>
2022-09-10 13:08:09 +0800

    persistence count

--- app.py
+++ app.py
@@ -18,8 +18,4 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return "What's up Docker Deep Divers! You've visited me {} times.\n".format(count)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    return f"Hey! You have smashed me {get_hit_count()} times!"
